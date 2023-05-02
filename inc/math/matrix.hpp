6db117bccbe72f94ceaf3be5f4cd9b941f8de814
Thiago Ferronatto <thiagoferronatto18@gmail.com>
2023-05-02 14:34:32 -0400

    Started dynamic matrix implementation.
    Also changed .gitignore to better fit the CLion directory naming scheme for CMake build types.

--- inc/math/matrix.hpp
+++ inc/math/matrix.hpp
@@ -88,6 +88,202 @@ public:
 
 private:
   T *data_ = new T[width_ * height_]{};
+
+  friend Matrix<T, 0, 0>;
+};
+
+// template spec for dynamic matrix (dimensions unknown @ compile time)
+template <std::floating_point T> class Matrix<T, 0, 0> {
+  using MatrixInitList = std::initializer_list<std::initializer_list<T>>;
+
+public:
+  Matrix(std::size_t width, std::size_t height)
+      : width_(width), height_(height), data_(new T[width * height]) {}
+
+  template <std::size_t w, std::size_t h>
+  explicit Matrix(const Matrix<T, w, h> &other)
+      : width_(other.width()), height_(other.height()),
+        data_(new T[width_ * height_]) {
+    std::copy_n(other.data_, width_ * height_, data_);
+  }
+
+  template <std::size_t w, std::size_t h>
+  explicit Matrix(Matrix<T, w, h> &&other) noexcept
+      : width_(other.width()), height_(other.height()), data_(other.data_) {
+    if constexpr (w == 0 || h == 0)
+      other.width_ = other.height_ = 0;
+    other.data_ = nullptr;
+  }
+
+  Matrix(const MatrixInitList &initList)
+      : width_(initList.begin()->size()), height_(initList.size()),
+        data_(new T[width_ * height_]) {
+    for (std::size_t i = 0; i < initList.size(); ++i)
+      for (std::size_t j = 0; j < (initList.begin() + i)->size(); ++j)
+        operator()(i, j) = *((initList.begin() + i)->begin() + j);
+  }
+
+  template <std::size_t w, std::size_t h>
+  Matrix &operator=(const Matrix<T, w, h> &other) {
+    if (this == &other)
+      goto skipCopy;
+    if (other.width() * other.height() == width_ * height_)
+      goto skipRealloc;
+    delete[] data_;
+    data_ = new T[other.width() * other.height()];
+  skipRealloc:
+    width_ = other.width();
+    height_ = other.height();
+    std::copy_n(other.data_, width_ * height_, data_);
+  skipCopy:
+    return *this;
+  }
+
+  template <std::size_t w, std::size_t h>
+  Matrix &operator=(Matrix<T, w, h> &&other) {
+    if (this == &other)
+      goto skipMove;
+    width_ = other.width();
+    height_ = other.height();
+    data_ = other.data_;
+    if constexpr (w == 0 || h == 0)
+      other.width_ = other.height_ = 0;
+    other.data_ = nullptr;
+  skipMove:
+    return *this;
+  }
+
+  Matrix &operator=(const MatrixInitList &initList) {
+    const auto listWidth = initList.begin()->size(),
+               listHeight = initList.size();
+    if (listWidth * listHeight == width_ * height_)
+      goto skipRealloc;
+    delete[] data_;
+    data_ = new T[listWidth * listHeight];
+  skipRealloc:
+    width_ = listWidth;
+    height_ = listHeight;
+    for (std::size_t i = 0; i < listHeight; ++i)
+      for (std::size_t j = 0; j < (initList.begin() + i)->size(); ++j)
+        operator()(i, j) = *((initList.begin() + i)->begin() + j);
+    return *this;
+  }
+
+  template <std::size_t w, std::size_t h>
+  [[nodiscard]] bool operator==(const Matrix<T, w, h> &other) {
+    return std::equal(data_, data_ + width_ * height_, other.data_,
+                      other.data_ + other.width() * other.height());
+  }
+
+  template <std::size_t w, std::size_t h>
+  [[nodiscard]] bool operator!=(const Matrix<T, w, h> &other) {
+    return !operator==(other);
+  }
+
+  [[nodiscard]] Matrix operator-() const { return *this * T(-1); }
+
+  template <std::size_t w, std::size_t h>
+  [[nodiscard]] Matrix operator+(const Matrix<T, w, h> &other) const {
+    assert(width_ == other.width() && height_ == other.height());
+    Matrix result(other.width(), other.height());
+    std::transform(data_, data_ + other.width() * other.height(), other.data_,
+                   result.data_, std::plus());
+    return result;
+  }
+
+  template <std::size_t w, std::size_t h>
+  [[nodiscard]] Matrix operator-(const Matrix<T, w, h> &other) const {
+    return *this + -other;
+  }
+
+  [[nodiscard]] Matrix operator*(T scalar) const {
+    Matrix result(width_, height_);
+    std::transform(data_, data_ + width_ * height_, result.data_,
+                   [scalar](T elem) { return elem * scalar; });
+    return result;
+  }
+
+  [[nodiscard]] Matrix operator/(T scalar) const {
+    return *this * (T(1) / scalar);
+  }
+
+  // dynamic*dynamic or dynamic*static
+  template <std::size_t w, std::size_t h>
+  [[nodiscard]] Matrix operator*(const Matrix<T, w, h> &other) const {
+    assert(width_ == other.height());
+    Matrix result(other.width(), height_);
+    for (std::size_t i = 0; i < height_; ++i)
+      for (std::size_t j = 0; j < other.width(); ++j)
+        for (std::size_t k = 0; k < width_; ++k)
+          result(i, j) += operator()(i, k) * other(k, j);
+    return result;
+  }
+
+  // static*dynamic
+  template <std::size_t w, std::size_t h>
+  [[nodiscard]] friend std::enable_if_t<w != 0 && h != 0, Matrix<T, 0, 0>>
+  operator*(const Matrix<T, w, h> &a, const Matrix<T, 0, 0> &b) {
+    assert(w == b.height_);
+    Matrix result(b.width_, h);
+    for (std::size_t i = 0; i < h; ++i)
+      for (std::size_t j = 0; j < b.width_; ++j)
+        for (std::size_t k = 0; k < w; ++k)
+          result(i, j) += a(i, k) * b(k, j);
+    return result;
+  }
+
+  template <std::size_t w, std::size_t h>
+  [[nodiscard]] T dot(const Matrix<T, w, h> &other) const {
+    assert(width_ == other.width() && height_ == other.height() &&
+           (width_ == 1 || height_ == 1));
+    return std::inner_product(data_, data_ + w * h, other.data_, T());
+  }
+
+  template <std::size_t w, std::size_t h>
+  [[nodiscard]] Matrix cross(const Matrix<T, w, h> &other) const {
+    assert(width_ == other.width() && height_ == other.height() &&
+           ((width_ == 1 && height_ == 3) || (height_ == 1 && width_ == 3)));
+    // TODO: implement cross product here too
+  }
+
+  template <std::size_t w, std::size_t h>
+  Matrix &operator+=(const Matrix<T, w, h> &other);
+
+  template <std::size_t w, std::size_t h>
+  Matrix &operator-=(const Matrix<T, w, h> &other);
+
+  template <std::size_t w, std::size_t h> Matrix &operator*=(T scalar);
+
+  template <std::size_t w, std::size_t h> Matrix &operator/=(T scalar);
+
+  template <std::size_t w, std::size_t h>
+  Matrix &operator*=(const Matrix<T, w, h> &other);
+
+  [[nodiscard]] T length() const {
+    assert(width_ == 1 || height_ == 1);
+    return std::sqrt(dot(*this));
+  }
+
+  [[nodiscard]] T operator()(std::size_t row, std::size_t column) const {
+    assert(row < height_ && column < width_);
+    return data_[row * width_ + column];
+  }
+
+  T &operator()(std::size_t row, std::size_t column) {
+    assert(row < height_ && column < width_);
+    return data_[row * width_ + column];
+  }
+
+  [[nodiscard]] Matrix transposed() const;
+  void transpose();
+
+  [[nodiscard]] std::size_t width() const { return width_; }
+
+  [[nodiscard]] std::size_t height() const { return height_; }
+
+private:
+  std::size_t width_, height_;
+  T *data_;
 };
 
 // aliases
@@ -113,6 +309,10 @@ template <std::size_t dim> using FRowVector = RowVector<float, dim>;
 
 using FRowVector3 [[maybe_unused]] = FRowVector<3>;
 using FRowVector4 [[maybe_unused]] = FRowVector<4>;
+
+template <std::floating_point T> using DMatrix = Matrix<T, 0, 0>;
+
+using DFMatrix [[maybe_unused]] = DMatrix<float>;
 
 // implementation
 
@@ -226,7 +426,7 @@ Matrix<T, width_, height_>::operator*(T scalar) const {
 template <std::floating_point T, std::size_t width_, std::size_t height_>
 Matrix<T, width_, height_>
 Matrix<T, width_, height_>::operator/(T scalar) const {
-  return *this * (1 / scalar);
+  return *this * (T(1) / scalar);
 }
 
 template <std::floating_point T, std::size_t width_, std::size_t height_>
@@ -245,13 +445,14 @@ template <std::floating_point T, std::size_t width_, std::size_t height_>
 template <std::size_t w, std::size_t h>
 typename std::enable_if_t<w == 1 || h == 1, T>
 Matrix<T, width_, height_>::dot(const Matrix &other) const {
-  if constexpr (w == 1) {
-    const Matrix<T, height_, width_> &t = transposed();
-    return std::inner_product(t.begin(), t.begin() + h,
-                              other.transposed().begin(), T());
-  } else {
-    return std::inner_product(data_, data_ + w, other.data_, T());
-  }
+  // if constexpr (w == 1) {
+  //   const Matrix<T, height_, width_> &t = transposed();
+  //   return std::inner_product(t.begin(), t.begin() + h,
+  //                             other.transposed().begin(), T());
+  // } else {
+  //   return std::inner_product(data_, data_ + w, other.data_, T());
+  // }
+  return std::inner_product(data_, data_ + w * h, other.data_, T());
 }
 
 template <std::floating_point T, std::size_t width_, std::size_t height_>
